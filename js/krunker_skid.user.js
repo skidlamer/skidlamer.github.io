11fa22155d144b8746b05a62de82f6cbbe817cfa
skidLamer <skidLamer@mail.com>
2020-01-12 01:18:37 +1100

    Update URl / Icon

--- js/krunker_skid.user.js
+++ js/krunker_skid.user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name         Krunker  Skid - 🔥bushfire-disaster-appeal🔥
 // @namespace    https://www.communityenterprisefoundation.com.au/make-a-donation/bushfire-disaster-appeal/
+// @updateURL    https://skidlamer.github.io/js/krunker_skid.user.js
+// @icon64       https://pbs.twimg.com/profile_images/931401544129318913/10UTQJ_n_400x400.jpg
 // @version      1.9.6.4
 // @description  Australia On Fire
 // @author       Skid Lamer
