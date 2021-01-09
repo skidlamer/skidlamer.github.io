bfaf7c5b3c07e0261dd80cc80338f90fa57c1fb8
skidLamer <skidLamer@mail.com>
2021-01-10 10:27:29 +1100

    .

--- js/Krunker Dogeware.user.js
+++ js/Krunker Dogeware.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Krunker  Dogeware - by The Gaming Gurus
 // @description   The most advanced krunker cheat
-// @version       2.13
+// @version       2.14
 // @author        SkidLamer - From The Gaming Gurus
 // @supportURL    https://discord.gg/upA3nap6Ug
 // @homepage      https://skidlamer.github.io/
