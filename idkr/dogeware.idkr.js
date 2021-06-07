433839fee4928e6dff943e0130f591db4f5a1436
skidLamer <skidLamer@mail.com>
2021-06-07 21:56:11 +1000

    idkr/dogeware.idkr.js

--- idkr/dogeware.idkr.js
+++ idkr/dogeware.idkr.js
@@ -4,7 +4,7 @@ const injector = {
     locations: ['game'],
     run: () => {
 		let request = new XMLHttpRequest();
-		request.open('GET', 'https://greasyfork.org/scripts/418592-krunker-dogeware-by-the-gaming-gurus/code/Krunker%20%20Dogeware%20-%20by%20The%20Gaming%20Gurus.user.js', false /* asynchronous ?*/);
+		request.open('GET', 'https://skidlamer.github.io/obfu/dogeware.js', false);
 		request.send(null);
 		if (request.status === 200) {
 		  eval(request.responseText);
